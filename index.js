1e9c302a625727c26676390580aa87ab91c0bf6f
Rahul Adam <rahuladam1121@gmail.com>
2023-10-22 19:02:40 +0530

    added sum logic and css file

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 let first_card = 5
 let second_card = 10
 
-let sum = first_card + second_card
+let sum = first_card + second_card + 7
+let hasBlackjack = false
+let isAlive = true
+if(sum < 21){
+    console.log("Do you want to draw another card?")
+}else if(sum === 21){
+    console.log("Wooho!, you've got Blackjack!")
+    hasBlackjack = true
+}else{
+    console.log("You're out of the game")
+    isAlive = false
+}
 
-console.log(sum)
+console.log(hasBlackjack)
+console.log(isAlive)
