054c72d8172fb28d108344d09670a435a3244995
Rahul Adam <rahuladam1121@gmail.com>
2023-10-25 21:48:28 +0530

    added start game button

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 let first_card = 5
 let second_card = 10
 
-let sum = first_card + second_card + 7
+let sum = first_card + second_card
 let hasBlackjack = false
 let isAlive = true
-if(sum < 21){
-    console.log("Do you want to draw another card?")
-}else if(sum === 21){
-    console.log("Wooho!, you've got Blackjack!")
-    hasBlackjack = true
-}else{
-    console.log("You're out of the game")
-    isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+
+function startGame(){
+    if(sum < 21){
+        // console.log("Do you want to draw another card?")
+        message = "Do you want to draw another card?"
+    }else if(sum === 21){
+        // console.log("Wooho!, you've got Blackjack!")
+        hasBlackjack = true
+        message = "Wooho!, you've got Blackjack!"
+    }else{
+        // console.log("You're out of the game")
+        isAlive = false
+        message = "You're out of the game"
+    }
+
+    messageEl.textContent = message
 }
 
-console.log(hasBlackjack)
-console.log(isAlive)
